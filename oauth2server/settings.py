9217dc024b1cbc50fe231b67480c9bc10d9b446a
Vivek <vivek@PDM4201u.INDIA.AVALON.NET>
2018-03-02 15:44:47 +0530

    database dir added

--- oauth2server/settings.py
+++ oauth2server/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'oauth2server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+DB_DIR = os.path.join(BASE_DIR, 'AppData')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DB_DIR, 'db.sqlite3'),
     }
 }
 
